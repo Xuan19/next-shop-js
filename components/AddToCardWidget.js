c62116a08c14b4069aabed3a27823a5afcaa6619
Xuan19 <xuanwang602@yahoo.com>
2022-02-10 19:13:33 +0100

    Add to Cart Mutation

--- components/AddToCardWidget.js
+++ components/AddToCardWidget.js
@@ -1,12 +1,23 @@
 import { useState } from "react";
 import Button from "./Button";
+import { useRouter } from 'next/router';
+import { useMutation} from 'react-query';
+import { fetchJson } from '../lib/api';
 
 export default function AddToCardWidget({ productID }) {
+  
+  const router = useRouter();
   const [quantity, setQuantity] = useState(1);
-  console.log(quantity);
 
-  const handleClick = (evt) => {
-    console.log(quantity, productID);
+  const mutation = useMutation(() => fetchJson('/api/cart', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ quantity, productID  }),
+    }));
+
+  const handleClick = async (evt) => {
+    await mutation.mutateAsync();
+    router.push('/cart')
   };
 
   return (
@@ -18,7 +29,9 @@ export default function AddToCardWidget({ productID }) {
         value={quantity.toString()}
         onChange={(event) => setQuantity(event.target.value)}
       />
-      <Button onClick={handleClick}>Add to Chart</Button>
+      {mutation.isLoading ? (<p>Loading...</p>) :
+          (<Button onClick={handleClick}>Add to Chart</Button>)
+        }
     </div>
   );
 }
